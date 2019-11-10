2bc2dbae7aefb68772f57cf26326a216a0b10c11
krushnatkhawale@gmail.com <krushnatkhawale@gmail.com>
2019-11-10 18:42:46 +0000

    concept-map ui working now:

---
+++ src/main/java/com/dependency/explorer/controller/ConceptMapController.java
@@ -0,0 +1,24 @@
+package com.dependency.explorer.controller;
+
+import com.dependency.explorer.model.BuildInfo;
+import com.dependency.explorer.service.BuildInfoService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BuildInfoController {
+
+    private final BuildInfoService buildInfoService;
+
+    public BuildInfoController(BuildInfoService buildInfoService) {
+        this.buildInfoService = buildInfoService;
+    }
+
+    @PostMapping("/buildInfo")
+    public ResponseEntity saveBuildInfo(@RequestBody BuildInfo buildInfo) {
+        buildInfoService.saveBuildInfo(buildInfo);
+        return ResponseEntity.ok().build();
+    }
+}
