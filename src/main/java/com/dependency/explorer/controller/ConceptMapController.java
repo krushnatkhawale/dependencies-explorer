41f0f03af6e1f75c4186fc1cba432fedfc9307ac
krushnatkhawale@gmail.com <krushnatkhawale@gmail.com>
2019-11-15 21:22:51 +0000

    concept map controller added

--- src/main/java/com/dependency/explorer/controller/ConceptMapController.java
+++ src/main/java/com/dependency/explorer/controller/ConceptMapController.java
@@ -3,22 +3,22 @@ package com.dependency.explorer.controller;
 import com.dependency.explorer.model.BuildInfo;
 import com.dependency.explorer.service.BuildInfoService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class BuildInfoController {
+public class ConceptMapController {
 
     private final BuildInfoService buildInfoService;
 
-    public BuildInfoController(BuildInfoService buildInfoService) {
+    public ConceptMapController(BuildInfoService buildInfoService) {
         this.buildInfoService = buildInfoService;
     }
 
-    @PostMapping("/buildInfo")
-    public ResponseEntity saveBuildInfo(@RequestBody BuildInfo buildInfo) {
-        buildInfoService.saveBuildInfo(buildInfo);
-        return ResponseEntity.ok().build();
+    @GetMapping("/conceptMap")
+    public ResponseEntity getConceptMap() {
+        return ResponseEntity.ok(buildInfoService.getConceptMap());
     }
 }
